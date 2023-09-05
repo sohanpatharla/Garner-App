dd24f7f0bb50fe5573bc092270f7370571226147
sohanpatharla <117771862+sohanpatharla@users.noreply.github.com>
2023-09-05 19:17:04 +0530

    jsx components files are added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const date = new Date();
+function Footer() {
+    return (<footer>
+    <p>Copyright ⓒ {date.getFullYear()}</p>
+    </footer>
+   )
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+ function Note() {
+    return <div class="note">
+        <h1 >This is note heading.</h1>
+        <p >This is note content.</p>
+    </div>
+ }
+ export default Note;
